b4d502d24b0421878f137d6411beeb8b64737c86
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-02-08 08:29:39 +0700

    test REST

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
+type FoodMenu struct {
+	Name		string
+	OrderCode 	string
+	Price		int
+}
+
 func getFoodMenu(c echo.Context) error {
-	return nil
+	foodMenu := []FoodMenu{
+		{
+			Name: "Rendang",
+			OrderCode: "Padang",
+			Price: 1000000,
+		},
+
+		{
+			Name: "AyamBakar",
+			OrderCode: "Warteg",
+			Price: 100000,
+		},
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"data": foodMenu,
+	})
 }
 
 func main()  {
 	e := echo.New()
 	e.GET("/menu/food", getFoodMenu)
-	e.Logger.Fatal(e.Start(":14045"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
