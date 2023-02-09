3e3bee50d33927eed793cd23397c2dc5c6958c4c
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-02-09 12:25:27 +0700

    Test PostgreSQL

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+)
+
+const (
+	dbAddress = "host=localhost port=5432 user=postgres password=mbangg12 dbname=go_resto_sim sslmode=disable"
 )
 
 func main()  {
@@ -17,10 +20,18 @@ func main()  {
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
+type MenuType string
+
+const (
+	MenuTypeFood = "food"
+	MenuTypeDrink = "drink"
+)
+
 type MenuItem struct {
 	Name		string
 	OrderCode 	string
 	Price		int64
+	Type		MenuType
 }
 
 
@@ -30,12 +41,14 @@ func seedDB()  {
 			Name: "Rendang",
 			OrderCode: "Padang",
 			Price: 1000000,
+			Type: MenuTypeFood,
 		},
 
 		{
 			Name: "AyamBakar",
 			OrderCode: "Warteg",
 			Price: 100000,
+			Type: MenuTypeFood,
 		},
 	}
 
@@ -44,34 +57,52 @@ func seedDB()  {
 			Name: "Bobba Gum",
 			OrderCode: "Bukan Soda",
 			Price: 1020004,
+			Type: MenuTypeDrink,
 		},
 
 		{
 			Name: "Orang Tua",
 			OrderCode: "Alkohol",
 			Price: 90000,
+			Type: MenuTypeDrink,
 		},
 	}
 
-	fmt.Println(foodMenu,drinkMenu)
-
-	dbAddress := "host=localhost port=5432 user=iqbalp12 password=mbangg12 dbname=go_resto_sim sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dbAddress))
 	if err != nil {
 		panic(err)
 	}
 
-	db.AutoMigrate(&MenuItem{})
+	if err := db.First(&MenuItem{}).Error; err == gorm.ErrRecordNotFound {
+		db.Create(&foodMenu)
+		db.Create(&drinkMenu)
+	}
 }
 
 func getFoodMenu(c echo.Context) error {
+	db, err := gorm.Open(postgres.Open(dbAddress))
+	if err != nil{
+		panic(err)
+	}
+
+	var menuData []MenuItem
+	db.Where(MenuItem{Type: MenuTypeFood}).Find(&menuData)
+
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		// "data": foodMenu,
+		"data": menuData,
 	})
 }
 
 func getDrinkMenu(c echo.Context) error{
+	db, err := gorm.Open(postgres.Open(dbAddress))
+	if err != nil {
+		panic(err)
+	}
+
+	var menuData []MenuItem
+	db.Where(MenuItem{Type: MenuTypeDrink}).Find(&menuData)
+
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		// "data": drinkMenu,
+		"data": menuData,
 	})
 }
